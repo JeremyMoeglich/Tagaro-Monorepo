a6d7c43f629ac09555fa06a8d2d8c8840d045553
MP Play <jeremy.moeglich@gmail.com>
2021-09-29 11:36:23 +0200

    added type annotiations to css_vars

--- find_blackened.py
+++ find_blackened.py
@@ -3,7 +3,7 @@ from typing import Iterable
 import PIL.Image
 import numpy as np
 from copy import deepcopy
-rootdir = "./static"
+rootdir = "./src"
 
 
 def is_black(img):
@@ -17,13 +17,21 @@ def is_black(img):
 
     return hsv == hsv2
 
+chars = 0
+total_files = 0
+
 for dirpath, _, files in os.walk(rootdir):
     for subfile in files:
-        if subfile[subfile.rfind("."):] not in {".png", ".jpg"}:
-            continue
+        #if subfile[subfile.rfind("."):] not in {".png", ".jpg"}:
+        #    continue
         img_path = os.path.join(dirpath, subfile)
-        selected_image = PIL.Image.open(img_path)
-        if is_black(selected_image):
-            print(img_path)
+        #selected_image = PIL.Image.open(img_path)
+        #if is_black(selected_image):
+        #    print(img_path)
+        total_files += 1
+        chars += len(open(img_path, "r").read().split(" "))
+
+print(chars, total_files)
+
     
         
