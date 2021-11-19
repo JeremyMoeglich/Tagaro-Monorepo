dd105f61821595bfb3d8c6cca75075d9ffcfe1fe
MP Play <jeremy.moeglich@gmail.com>
2021-11-19 16:13:26 +0100

    added nbsp

--- src/lib/site_components/Showcases/Cyberweek.svelte
+++ src/lib/site_components/Showcases/Cyberweek.svelte
@@ -4,14 +4,17 @@
 
 <div class="alignment">
 	<img class="main" src="/images/visuals/cyberweek/sky_21-11_cyberweek_home_text_s.webp" alt="" />
+	
 	<Button
 		text="Jetzt Bestellen"
 		route="https://fd10.formdesk.com/tagaromedienshopgbr/Sky-Bestellung-1"
 	/>
+
 	<h2>
-		Hol dir alles von Sky für nur € 42,50 mtl. und Sky schenkt dir einen 32" Toshiba Full HD
+		Hol dir alles von Sky für nur €&nbsp42,50 mtl. und Sky schenkt dir einen 32" Toshiba Full HD
 		LED-TV.*
 	</h2>
+	<h2 class="bonus">+ €&nbsp20 Bonus zusätzlich </h2>
 	<div class="outer_box">
 		<h3>32" Toshiba Full HD LED-TV 32L3163DAS geschenkt*:</h3>
 		<ul>
@@ -42,7 +45,7 @@
 	</div>
 	<div class="outer_box">
 		<h3>
-			Du hast schon einen TV? Dann sichere dir alternativ einen Amazon Gutschein im Wert von € 150.
+			Du hast schon einen TV? Dann sichere dir alternativ einen Amazon Gutschein im Wert von €&nbsp150.
 		</h3>
 		<div class="amazon_alignment">
 			<ul>
@@ -50,7 +53,8 @@
 				<li>Sky Entertainment und Sky Q immer inklusive</li>
 				<li>Auf Wunsch auch inklusive Netflix</li>
 				<li>Mit der Sky Go App Sky auch von unterwegs erleben</li>
-				<li>Inklusive Amazon Gutschein im Wert von € 150</li>
+				<li>Inklusive Amazon Gutschein im Wert von €&nbsp150</li>
+				<li>€&nbsp20 Bonus zusätzlich</li>
 			</ul>
 			<img class="sub" src="/images/visuals/cyberweek/sky_21-11_amazon-karte_16-9_v2.webp" alt="" />
 		</div>
@@ -68,6 +72,9 @@
 		max-width: 90vw;
 		width: 1000px;
 		padding: 20px 0px;
+	}
+	.bonus {
+		margin: 0px;
 	}
 	img.sub {
 		max-width: 50%;
