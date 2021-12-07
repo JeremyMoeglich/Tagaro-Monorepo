47c27a044b6d1024fe933f3fa1c2bab7ffece18e
MP Play <jeremy.moeglich@gmail.com>
2021-12-07 09:44:04 +0100

    changed to default showcase

--- src/lib/site_components/main_showcase.svelte
+++ src/lib/site_components/main_showcase.svelte
@@ -2,7 +2,8 @@
 	//import TvnowShowcase from './Showcases/tvnow_showcase.svelte';
 	//import Amazon20Showcase from './Showcases/Amazon20_showcase.svelte';
 	//import GratisShowcase from './Showcases/Gratis_showcase.svelte';
-	import Cyberweek from "./Showcases/Cyberweek.svelte";
+	//import Cyberweek from "./Showcases/Cyberweek.svelte";
+	import DefaultShowcase from './Showcases/default_showcase.svelte';
 </script>
 
-<Cyberweek />
+<DefaultShowcase />
