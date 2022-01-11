3ad688ddc5468da52c92c51a8f33604eb9a94d2d
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-01-11 17:25:37 +0100

    changed pnpm install 2

--- src/lib/site_components/footer.svelte
+++ src/lib/site_components/footer.svelte
@@ -61,6 +61,7 @@
 	{/if}
 </div>
 <p class="copyright">© 2022 - TAGARO Medienshop Möglich & Möglich GbR</p>
+<p class="notice">Alle Sky Marken sind Eigentum der Sky International AG und werden von der Sky Deutschland Fernsehen GmbH & Co. KG in Lizenz genutzt. Alle hier verwendeten Namen, Begriffe, Zeichen, Logos und Grafiken können Marken- oder Warenzeichen im Besitze ihrer rechtlichen Eigentümer sein und werden von uns ausschließlich zu Vermittlungs- und Visualisierungszwecken der einzelnen Abonnement-Angebote verwendet.</p>
 
 <style lang="scss">
 	.side_alignment {
@@ -104,6 +105,15 @@
 	.copyright {
 		text-align: center;
 		margin-top: 30px;
+	}
+	.notice {
+		text-align: center;
 		margin-bottom: 90px;
+		font-size: small;
+		color: #545454;
+		max-width: 80%;
+		width: 500px;
+		margin-left: auto;
+		margin-right: auto;
 	}
 </style>
