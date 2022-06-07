29fd1271c515958a3306297d70e5acc9bb51df16
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-06-08 00:39:38 +0200

    improve email log

--- src/routes/admin/email_log.svelte
+++ src/routes/admin/email_log.svelte
@@ -21,15 +21,15 @@
 	let email_log: email_log_type[] = [];
 
 	onSnapshot(
-		query(collection(firestore, 'email_log'), orderBy('timestamp', 'desc'), limit(30)),
+		query(collection(firestore, 'email_log'), orderBy('timestamp', 'desc'), limit(12)),
 		(snapshot) => {
 			console.log(snapshot.docs.map((doc) => doc.data()));
 			email_log = sortBy(
 				snapshot.docs.map((doc) => {
-                    const data = doc.data()
-                    data.timestamp = new Date(data.timestamp.seconds * 1000)
-                    return data as email_log_type
-                }),
+					const data = doc.data();
+					data.timestamp = new Date(data.timestamp.seconds * 1000);
+					return data as email_log_type;
+				}),
 				(item) => item.timestamp
 			);
 		}
@@ -38,13 +38,35 @@
 
 <div class="outer">
 	<h2>Email Log</h2>
-	<div class="values">
+	<table class="values">
 		{#each email_log as item}
-			<div>
-				<p>
-					{item.email} - {item.source} - {item.title} - {item.timestamp.toLocaleString('de-De')}
-				</p>
-			</div>
+			<tr class="log">
+				<td>
+					{item.email}
+				</td>
+				<td>
+					{item.source}
+				</td>
+				<td>
+					{item.timestamp.toLocaleString('de-De')}
+				</td>
+				<td>
+					{item.title}
+				</td>
+			</tr>
 		{/each}
-	</div>
+	</table>
 </div>
+
+<style>
+	.log {
+		width: 100%;
+		max-width: 1300px;
+		padding: 15px;
+		border-radius: 5px;
+		background-color: rgb(230, 234, 240);
+	}
+	td {
+		padding: 5px;
+	}
+</style>
