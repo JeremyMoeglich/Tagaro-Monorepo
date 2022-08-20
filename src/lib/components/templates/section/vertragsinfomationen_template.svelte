6a6bf75c06f4a8b1e1b8cd78d8aa60a6c26126e9
Jeremy Möglich <jeremy.moeglich@gmail.com>
2022-08-20 22:25:45 +0200

    format

--- src/lib/components/templates/section/vertragsinfomationen_template.svelte
+++ src/lib/components/templates/section/vertragsinfomationen_template.svelte
@@ -72,10 +72,8 @@
 					<li><b>Optional:</b></li>
 					<li>- UHD mtl. {get_price_string(['uhd'], 'jahr')} (für Sat oder Kabel-Empfang)</li>
 					<li>
-						- DAZN jährlich für {get_price_string(['dazn_yearly'], 'jahr')} mtl. oder DAZN monatlich für {get_price_string(
-							['dazn_monthly'],
-							'monat'
-						)} mtl.
+						- DAZN jährlich für {get_price_string(['dazn_yearly'], 'jahr')} mtl. oder DAZN monatlich
+						für {get_price_string(['dazn_monthly'], 'monat')} mtl.
 					</li>
 					<li>
 						- Multiscreen für {get_price_string(['multiscreen'], 'jahr')} mtl. - Inkl. Sky Go Plus für
