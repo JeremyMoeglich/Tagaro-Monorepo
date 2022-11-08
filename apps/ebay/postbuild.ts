a421679bd6b9bd90ad7ab374e084aed4e4ff6265
jeremy <jeremy@moeglich.dev>
2022-11-08 14:23:47 +0100

    remove javascript from ebay

--- apps/ebay/postbuild.ts
+++ apps/ebay/postbuild.ts
@@ -1,19 +1,32 @@
 import htmlInlineExternal from 'html-inline-external';
 import { promises as fs } from 'fs';
 import { join } from 'path';
+import { stripHtml } from 'string-strip-html';
 
 const build_folder = 'build';
 const build_path = join(import.meta.url, '..', build_folder).replace('file:', '');
 
-console.log(`Inlining external resources in ${build_folder} folder...`);
-
 (async () => {
+	console.log(`Inlining external resources in ${build_folder} folder...`);
+
 	for (const file of await fs.readdir(build_path)) {
 		if (file.endsWith('.html')) {
 			const file_path = join(build_path, file);
 			const inlined = await htmlInlineExternal({ src: file_path, minify: true });
 			await fs.writeFile(file_path, inlined);
 			console.log(`Inlined ${file}`);
+		}
+	}
+
+	console.log(`Striping JS from ${build_folder} folder...`);
+
+	for (const file of await fs.readdir(build_path)) {
+		if (file.endsWith('.html')) {
+			const file_path = join(build_path, file);
+			const html = await fs.readFile(file_path, 'utf-8');
+			const stripped = stripHtml(html, { onlyStripTags: ['script'] });
+			await fs.writeFile(file_path, stripped.result);
+			console.log(`Stripped JS from ${file}`);
 		}
 	}
 })();
