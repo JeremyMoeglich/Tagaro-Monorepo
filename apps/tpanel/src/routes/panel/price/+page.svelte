caa1e809d42c66850084ce688852674be92129c3
jeremy <jeremy@moeglich.dev>
2024-01-16 15:32:56 +0100

    update internal prices

--- apps/tpanel/src/routes/panel/price/+page.svelte
+++ apps/tpanel/src/routes/panel/price/+page.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import { package_combinations } from 'asset_library/assets/packages';
+	import { empty_offer } from 'asset_library/offer_description';
 
-	import { get_offer_note, get_price } from 'asset_library/prices';
+	import { get_offer_note, get_offer_price, get_price } from 'asset_library/prices';
 </script>
 
 <div class="flex flex-col p-8 text-slate-300">
 	{#each package_combinations as package_combination}
 		{@const price = get_price(package_combination)}
+		{@const default_price = get_offer_price(empty_offer, package_combination)}
 
 		<div>
 			<div>
-				{package_combination.join(', ')} = {@html price.jahr.toFixed(2)} => {@html price.monat.toFixed(
+				{package_combination.join(', ')} = {@html price.jahr.toFixed(2)} (default: {@html default_price.jahr.toFixed(
 					2
-				)}
+				)}) => {@html price.monat.toFixed(2)}
 				{@html get_offer_note(package_combination)}
 			</div>
 		</div>
--- packages/asset_library/assets/packages.ts
+++ packages/asset_library/assets/packages.ts
@@ -21,7 +21,7 @@ const packages_image_location = '/images/assets/packages/';
 export const packages_assets = [
 	{
 		id: 'entertainment',
-		price: { jahr: 16.5, monat: 19, singular: 0 },
+		price: { jahr: 17.5, monat: 19, singular: 0 },
 		name: 'Entertainment',
 		note: 'Exklusives Entertainment ohne Ende.',
 		image: {
@@ -67,7 +67,7 @@ export const packages_assets = [
 	},
 	{
 		id: 'cinema',
-		price: { jahr: 11, monat: 13.5, singular: 0 },
+		price: { jahr: 12.5, monat: 13.5, singular: 0 },
 		name: 'Cinema',
 		note: 'Jeden Tag ein neuer Film.',
 		image: {
@@ -90,7 +90,7 @@ export const packages_assets = [
 	},
 	{
 		id: 'sport',
-		price: { jahr: 6.5, monat: 9, singular: 0 },
+		price: { jahr: 7.5, monat: 9, singular: 0 },
 		name: 'Sport',
 		note: 'Der beste Live-Sport.',
 		image: {
@@ -112,7 +112,7 @@ export const packages_assets = [
 	},
 	{
 		id: 'bundesliga',
-		price: { jahr: 14, monat: 16.5, singular: 0 },
+		price: { jahr: 15, monat: 16.5, singular: 0 },
 		name: 'Bundesliga',
 		note: 'Die Heimat der Bundesliga - garantiert bis 2025.',
 		image: {
