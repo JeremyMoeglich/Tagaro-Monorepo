40142cd3a1661d66b4618e3449ca802ec4920020
JeremyMoeglich <jeremy@moeglich.dev>
2024-04-30 09:59:57 +0200

    change wording

--- packages/components/templates/section/vertragsinfomationen_template.svelte
+++ packages/components/templates/section/vertragsinfomationen_template.svelte
@@ -435,10 +435,10 @@
 	<div>
 		<h3 class={title_classes}>Zusätzliche Informationen:</h3>
 		<p>
-			Dieses Angebot gilt für Internet, Sat- oder Kabelanschluss. Es ist eine deutsche Adresse und
-			Bankverbindung zur Bestellung erforderlich. Eine Änderung auf eine andere Zahlweise ist später
-			in deinem Sky Kundencenter möglich. Der Abobeginn ist nicht verlegbar. Der Vertrag startet
-			innerhalb von 7 Tagen nach Erhalt der Ware.
+			Dieses Angebot gilt für Internet, Sat- oder Kabelanschluss. Du benötigst eine deutsche Adresse
+			und eine Bankverbindung für die Vertragsanmeldung. Eine Änderung auf eine andere Zahlungsweise
+			ist später in deinem Sky Kundencenter möglich. Der Abobeginn ist nicht verlegbar. Der Vertrag
+			startet innerhalb von 7 Tagen nach Erhalt der Ware.
 		</p>
 	</div>
 	<!-- <div>
